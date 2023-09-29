df7da6d11fc7adb13858aecbc183ad3dad56beb3
cachis <marcoscachis@gmail.com>
2023-09-28 22:59:45 -0600

    add show password & delete set cookie from here

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,31 +1,37 @@
 import { useRef, useState } from 'react';
 import { Divider } from '../components/divider'
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import GoogleIcon from '../assets/google.png';
 import FacebookIcon from '../assets/facebook.png';
 
 //services
 import { loginUser } from '../api/user.js';
-import  Cookies  from 'universal-cookie';
-const cookies = new Cookies(null, { path: '/' });
 
 const LoginPage = () => {
     const [error, setError] = useState(null)
     const emailRef = useRef();
     const passwordRef = useRef();
+    const navigate = useNavigate();
 
+    const togglePassword = () => {
+        if(passwordRef.current.type === 'password'){
+            passwordRef.current.type = 'text'
+        }else{
+            passwordRef.current.type = 'password'
+        }
+    }
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const email = emailRef.current.value;
             const password = passwordRef.current.value;
-            let response = await loginUser({email, password});
-            
+            let response = await loginUser({email, password})
             if(response.status != 200) return setError("Invalid email or password")
 
-            response = await response.json();
-            console.log(response.token);
-            cookies.set('_iduser', response.token, { path: '/' });
+            response = await response.json()
+            setError(null)  
+            navigate('/')
+            //cookies.set('_iduser', response.token, { path: '/' });
         } catch (error) {
             setError("Invalid email or password")
         }
@@ -37,11 +43,16 @@ const LoginPage = () => {
         <form className='mt-5' onSubmit={handleSubmit}>
             <div className='flex flex-col items-start'>
                 <label htmlFor='email' className='font-semibold'>Email address</label>
-                <input type="text" name="email" className='w-full border-2 border-pink-600 rounded-md h-10 mt-1 p-2' ref={emailRef}/>
+                <input type="text" name="email" className='w-full border-2 border-pink-600 rounded-md h-14 mt-1 p-2' ref={emailRef}/>
             </div>
             <div className='flex flex-col items-start mt-3'>
                 <label htmlFor="password" className='font-semibold'>Password</label>
-                <input type="password" name="password" className='w-full border-2 border-pink-600 rounded-md h-10 mt-1 p-2'ref={passwordRef} />
+                <input type="password" name="password" className='w-full border-2 border-pink-600 rounded-md h-14 mt-1 p-2'ref={passwordRef} />
+            </div>
+            {/* Show password */}
+            <div className='flex items-center mt-3'>
+                <input type="checkbox" name="showPassword" id="showPassword" onClick={togglePassword} className='w-4 h-4'/>
+                <label htmlFor="showPassword" className='ml-2'>Show password</label>
             </div>
             {error && <p className='text-red-600 text-sm mt-3 text-left'>{error}</p>}
             <button type="submit" className='rounded-md bg-pink-600 mt-5 text-white text-xl w-full'>Sign In</button>
