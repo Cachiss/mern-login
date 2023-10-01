6440ec50d4a1facfbc5daf168a14f7fa92792163
cachis <marcoscachis@gmail.com>
2023-09-30 18:36:12 -0600

    loading animation component

---
+++ client/src/components/loading.jsx
@@ -0,0 +1,12 @@
+import { ThreeBody as LoadingBody } from '@uiball/loaders'
+
+const ThreeBody = () => {
+  return (
+    <LoadingBody 
+     size={35}
+     speed={1.1} 
+     color="black" 
+    />  )
+}
+
+export default ThreeBody
